9ff5180b8787e6df81e5d20af8299da7dea99ba7
Alan Lengkoan <alanlengkoan15@gmail.com>
2023-10-13 13:24:45 +0800

    update again

--- routes/web.php
+++ routes/web.php
@@ -34,147 +34,147 @@ Route::group(
         Route::post('/check', [AuthController::class, 'check'])->name('auth.check');
         Route::get('/logout', [AuthController::class, 'logout'])->name('auth.logout');
 
-        Route::group(['middleware' => ['session.auth', 'prevent.back.history']], function () {
-            Route::controller(DashboardController::class)->as('dashboard.')->group(function () {
-                Route::get('/', [DashboardController::class, 'index'])->name('index');
-                Route::get('/count_visitors', [DashboardController::class, 'count_visitors'])->name('count_visitors');
-                Route::get('/count_visitors_loc', [DashboardController::class, 'count_visitors_loc'])->name('count_visitors_loc');
-            });
-
-            // begin:: profil
-            Route::controller(ProfilController::class)->prefix('profil')->as('profil.')->group(function () {
-                Route::get('/', 'index')->name('index');
-                Route::post('/save_picture', 'save_picture')->name('save_picture');
-                Route::post('/save_account', 'save_account')->name('save_account');
-                Route::post('/save_security', 'save_security')->name('save_security');
-            });
-            // end:: profil
-
-            // begin:: based
-            Route::controller(BasedController::class)->prefix('based')->as('based.')->group(function () {
-                Route::get('/', 'index')->name('index');
-                Route::get('/get_all', 'get_all')->name('get_all');
-                Route::get('/get_data_dt', 'get_data_dt')->name('get_data_dt');
-                Route::post('/show', 'show')->name('show');
-                Route::post('/save', 'save')->name('save');
-                Route::post('/del', 'del')->name('del');
-            });
-            // end:: based
-
-            // begin:: stack
-            Route::controller(StackController::class)->prefix('stack')->as('stack.')->group(function () {
-                Route::get('/', 'index')->name('index');
-                Route::get('/get_all', 'get_all')->name('get_all');
-                Route::get('/get_data_dt', 'get_data_dt')->name('get_data_dt');
-                Route::post('/show', 'show')->name('show');
-                Route::post('/save', 'save')->name('save');
-                Route::post('/del', 'del')->name('del');
-            });
-            // end:: stack
-
-            // begin:: type
-            Route::controller(TypeController::class)->prefix('type')->as('type.')->group(function () {
-                Route::get('/', 'index')->name('index');
-                Route::get('/get_all', 'get_all')->name('get_all');
-                Route::get('/get_data_dt', 'get_data_dt')->name('get_data_dt');
-                Route::post('/show', 'show')->name('show');
-                Route::post('/save', 'save')->name('save');
-                Route::post('/del', 'del')->name('del');
-            });
-            // end:: type
-
-            // begin:: price
-            Route::controller(PriceController::class)->prefix('price')->as('price.')->group(function () {
-                Route::get('/', 'index')->name('index');
-                Route::get('/get_all', 'get_all')->name('get_all');
-                Route::get('/get_data_dt', 'get_data_dt')->name('get_data_dt');
-                Route::post('/show', 'show')->name('show');
-                Route::post('/save', 'save')->name('save');
-                Route::post('/del', 'del')->name('del');
-            });
-            // end:: price
-
-            // begin:: social_media
-            Route::controller(SocialMediaController::class)->prefix('social_media')->as('social_media.')->group(function () {
-                Route::get('/', 'index')->name('index');
-                Route::get('/get_all', 'get_all')->name('get_all');
-                Route::get('/get_data_dt', 'get_data_dt')->name('get_data_dt');
-                Route::post('/show', 'show')->name('show');
-                Route::post('/save', 'save')->name('save');
-                Route::post('/del', 'del')->name('del');
-            });
-            // end:: social_media
-
-            // begin:: product
-            Route::controller(ProductController::class)->prefix('product')->as('product.')->group(function () {
-                Route::get('/', 'index')->name('index');
-                Route::get('/add', 'add')->name('add');
-                Route::get('/upd/{id}', 'upd')->name('upd');
-                Route::get('/det/{id}', 'det')->name('det');
-                Route::get('/get_all', 'get_all')->name('get_all');
-                Route::get('/get_data_dt', 'get_data_dt')->name('get_data_dt');
-                Route::get('/get_stack_detail/{id}', 'get_stack_detail')->name('get_stack_detail');
-                Route::get('/get_picture_detail/{id}', 'get_picture_detail')->name('get_picture_detail');
-                Route::post('/del_picture_detail', 'del_picture_detail')->name('del_picture_detail');
-                Route::post('/show', 'show')->name('show');
-                Route::post('/save', 'save')->name('save');
-                Route::post('/del', 'del')->name('del');
-            });
-            // end:: product
-
-            // begin:: project
-            Route::controller(ProjectController::class)->prefix('project')->as('project.')->group(function () {
-                Route::get('/', 'index')->name('index');
-                Route::get('/add', 'add')->name('add');
-                Route::get('/upd/{id}', 'upd')->name('upd');
-                Route::get('/det/{id}', 'det')->name('det');
-                Route::get('/get_all', 'get_all')->name('get_all');
-                Route::get('/get_data_dt', 'get_data_dt')->name('get_data_dt');
-                Route::get('/get_stack_detail/{id}', 'get_stack_detail')->name('get_stack_detail');
-                Route::get('/get_picture_detail/{id}', 'get_picture_detail')->name('get_picture_detail');
-                Route::post('/del_picture_detail', 'del_picture_detail')->name('del_picture_detail');
-                Route::post('/show', 'show')->name('show');
-                Route::post('/save', 'save')->name('save');
-                Route::post('/del', 'del')->name('del');
-            });
-            // end:: project
-
-            // begin:: contact
-            Route::controller(ContactController::class)->prefix('contact')->as('contact.')->group(function () {
-                Route::get('/', 'index')->name('index');
-                Route::get('/det/{id}', 'det')->name('det');
-                Route::get('/get_data_dt', 'get_data_dt')->name('get_data_dt');
-                Route::post('/del', 'del')->name('del');
-            });
-            // end:: contact
-
-            // begin:: testimony
-            Route::controller(TestimonyController::class)->prefix('testimony')->as('testimony.')->group(function () {
-                Route::get('/', 'index')->name('index');
-                Route::get('/det/{id}', 'det')->name('det');
-                Route::get('/get_data_dt', 'get_data_dt')->name('get_data_dt');
-                Route::post('/posting', 'posting')->name('posting');
-                Route::post('/del', 'del')->name('del');
-            });
-            // end:: testimony
-
-            // begin:: visitor
-            Route::controller(VisitorController::class)->prefix('visitor')->as('visitor.')->group(function () {
-                Route::get('/', 'index')->name('index');
-                Route::get('/get_data_dt', 'get_data_dt')->name('get_data_dt');
-            });
-            // end:: visitor
-
-            // begin:: notification
-            Route::controller(NotificationController::class)->prefix('notification')->as('notification.')->group(function () {
-                Route::get('/{status}', 'index')->name('index');
-                Route::post('/load', 'load')->name('load');
-                Route::post('/read', 'read')->name('read');
-                Route::post('/read_all', 'read_all')->name('read_all');
-                Route::post('/delete_all', 'delete_all')->name('delete_all');
-            });
-            // end:: notification
+        // Route::group(['middleware' => ['session.auth', 'prevent.back.history']], function () {
+        Route::controller(DashboardController::class)->as('dashboard.')->group(function () {
+            Route::get('/', [DashboardController::class, 'index'])->name('index');
+            Route::get('/count_visitors', [DashboardController::class, 'count_visitors'])->name('count_visitors');
+            Route::get('/count_visitors_loc', [DashboardController::class, 'count_visitors_loc'])->name('count_visitors_loc');
         });
+
+        // begin:: profil
+        Route::controller(ProfilController::class)->prefix('profil')->as('profil.')->group(function () {
+            Route::get('/', 'index')->name('index');
+            Route::post('/save_picture', 'save_picture')->name('save_picture');
+            Route::post('/save_account', 'save_account')->name('save_account');
+            Route::post('/save_security', 'save_security')->name('save_security');
+        });
+        // end:: profil
+
+        // begin:: based
+        Route::controller(BasedController::class)->prefix('based')->as('based.')->group(function () {
+            Route::get('/', 'index')->name('index');
+            Route::get('/get_all', 'get_all')->name('get_all');
+            Route::get('/get_data_dt', 'get_data_dt')->name('get_data_dt');
+            Route::post('/show', 'show')->name('show');
+            Route::post('/save', 'save')->name('save');
+            Route::post('/del', 'del')->name('del');
+        });
+        // end:: based
+
+        // begin:: stack
+        Route::controller(StackController::class)->prefix('stack')->as('stack.')->group(function () {
+            Route::get('/', 'index')->name('index');
+            Route::get('/get_all', 'get_all')->name('get_all');
+            Route::get('/get_data_dt', 'get_data_dt')->name('get_data_dt');
+            Route::post('/show', 'show')->name('show');
+            Route::post('/save', 'save')->name('save');
+            Route::post('/del', 'del')->name('del');
+        });
+        // end:: stack
+
+        // begin:: type
+        Route::controller(TypeController::class)->prefix('type')->as('type.')->group(function () {
+            Route::get('/', 'index')->name('index');
+            Route::get('/get_all', 'get_all')->name('get_all');
+            Route::get('/get_data_dt', 'get_data_dt')->name('get_data_dt');
+            Route::post('/show', 'show')->name('show');
+            Route::post('/save', 'save')->name('save');
+            Route::post('/del', 'del')->name('del');
+        });
+        // end:: type
+
+        // begin:: price
+        Route::controller(PriceController::class)->prefix('price')->as('price.')->group(function () {
+            Route::get('/', 'index')->name('index');
+            Route::get('/get_all', 'get_all')->name('get_all');
+            Route::get('/get_data_dt', 'get_data_dt')->name('get_data_dt');
+            Route::post('/show', 'show')->name('show');
+            Route::post('/save', 'save')->name('save');
+            Route::post('/del', 'del')->name('del');
+        });
+        // end:: price
+
+        // begin:: social_media
+        Route::controller(SocialMediaController::class)->prefix('social_media')->as('social_media.')->group(function () {
+            Route::get('/', 'index')->name('index');
+            Route::get('/get_all', 'get_all')->name('get_all');
+            Route::get('/get_data_dt', 'get_data_dt')->name('get_data_dt');
+            Route::post('/show', 'show')->name('show');
+            Route::post('/save', 'save')->name('save');
+            Route::post('/del', 'del')->name('del');
+        });
+        // end:: social_media
+
+        // begin:: product
+        Route::controller(ProductController::class)->prefix('product')->as('product.')->group(function () {
+            Route::get('/', 'index')->name('index');
+            Route::get('/add', 'add')->name('add');
+            Route::get('/upd/{id}', 'upd')->name('upd');
+            Route::get('/det/{id}', 'det')->name('det');
+            Route::get('/get_all', 'get_all')->name('get_all');
+            Route::get('/get_data_dt', 'get_data_dt')->name('get_data_dt');
+            Route::get('/get_stack_detail/{id}', 'get_stack_detail')->name('get_stack_detail');
+            Route::get('/get_picture_detail/{id}', 'get_picture_detail')->name('get_picture_detail');
+            Route::post('/del_picture_detail', 'del_picture_detail')->name('del_picture_detail');
+            Route::post('/show', 'show')->name('show');
+            Route::post('/save', 'save')->name('save');
+            Route::post('/del', 'del')->name('del');
+        });
+        // end:: product
+
+        // begin:: project
+        Route::controller(ProjectController::class)->prefix('project')->as('project.')->group(function () {
+            Route::get('/', 'index')->name('index');
+            Route::get('/add', 'add')->name('add');
+            Route::get('/upd/{id}', 'upd')->name('upd');
+            Route::get('/det/{id}', 'det')->name('det');
+            Route::get('/get_all', 'get_all')->name('get_all');
+            Route::get('/get_data_dt', 'get_data_dt')->name('get_data_dt');
+            Route::get('/get_stack_detail/{id}', 'get_stack_detail')->name('get_stack_detail');
+            Route::get('/get_picture_detail/{id}', 'get_picture_detail')->name('get_picture_detail');
+            Route::post('/del_picture_detail', 'del_picture_detail')->name('del_picture_detail');
+            Route::post('/show', 'show')->name('show');
+            Route::post('/save', 'save')->name('save');
+            Route::post('/del', 'del')->name('del');
+        });
+        // end:: project
+
+        // begin:: contact
+        Route::controller(ContactController::class)->prefix('contact')->as('contact.')->group(function () {
+            Route::get('/', 'index')->name('index');
+            Route::get('/det/{id}', 'det')->name('det');
+            Route::get('/get_data_dt', 'get_data_dt')->name('get_data_dt');
+            Route::post('/del', 'del')->name('del');
+        });
+        // end:: contact
+
+        // begin:: testimony
+        Route::controller(TestimonyController::class)->prefix('testimony')->as('testimony.')->group(function () {
+            Route::get('/', 'index')->name('index');
+            Route::get('/det/{id}', 'det')->name('det');
+            Route::get('/get_data_dt', 'get_data_dt')->name('get_data_dt');
+            Route::post('/posting', 'posting')->name('posting');
+            Route::post('/del', 'del')->name('del');
+        });
+        // end:: testimony
+
+        // begin:: visitor
+        Route::controller(VisitorController::class)->prefix('visitor')->as('visitor.')->group(function () {
+            Route::get('/', 'index')->name('index');
+            Route::get('/get_data_dt', 'get_data_dt')->name('get_data_dt');
+        });
+        // end:: visitor
+
+        // begin:: notification
+        Route::controller(NotificationController::class)->prefix('notification')->as('notification.')->group(function () {
+            Route::get('/{status}', 'index')->name('index');
+            Route::post('/load', 'load')->name('load');
+            Route::post('/read', 'read')->name('read');
+            Route::post('/read_all', 'read_all')->name('read_all');
+            Route::post('/delete_all', 'delete_all')->name('delete_all');
+        });
+        // end:: notification
+        // });
     }
 );
 
