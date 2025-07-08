1aac3ddbad779b391f4244078cd274a397b62bbc
Alan Lengkoan <alansaputralengkoan@gmail.com>
2025-07-08 16:34:12 +0800

    update again

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -62,15 +62,17 @@ class Kernel extends HttpKernel
      * @var array<string, class-string|string>
      */
     protected $routeMiddleware = [
-        'auth'                 => \App\Http\Middleware\Authenticate::class,
-        'auth.basic'           => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
-        'cache.headers'        => \Illuminate\Http\Middleware\SetCacheHeaders::class,
-        'can'                  => \Illuminate\Auth\Middleware\Authorize::class,
-        'guest'                => \App\Http\Middleware\RedirectIfAuthenticated::class,
-        'password.confirm'     => \Illuminate\Auth\Middleware\RequirePassword::class,
-        'signed'               => \Illuminate\Routing\Middleware\ValidateSignature::class,
-        'throttle'             => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'verified'             => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'auth'             => \App\Http\Middleware\Authenticate::class,
+        'auth.basic'       => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+        'cache.headers'    => \Illuminate\Http\Middleware\SetCacheHeaders::class,
+        'can'              => \Illuminate\Auth\Middleware\Authorize::class,
+        'guest'            => \App\Http\Middleware\RedirectIfAuthenticated::class,
+        'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
+        'signed'           => \Illuminate\Routing\Middleware\ValidateSignature::class,
+        'throttle'         => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'verified'         => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+
+        // begin:: created my middlewares
         'session.auth'         => \App\Http\Middleware\Auth::class,
         'prevent.back.history' => \App\Http\Middleware\PreventBackHistory::class,
         'set.locale'           => \App\Http\Middleware\SetLocale::class,
